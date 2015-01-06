d4c410a33dcb1bced6baeecdd0adc40f2437c4fb
VapourDrive <colin.g.bos@gmail.com>
2015-01-05 20:09:00 -0500

    Can now rip enchantments of of tools onto gems, huzzah!

--- src/main/java/com/vapourdrive/harderstart/events/HS_AnvilEvent.java
+++ src/main/java/com/vapourdrive/harderstart/events/HS_AnvilEvent.java
@@ -1,13 +1,19 @@
 package com.vapourdrive.harderstart.events;
 
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.AnvilUpdateEvent;
+
+import org.apache.logging.log4j.Level;
+
+import com.vapourdrive.harderstart.HarderStart;
+import com.vapourdrive.harderstart.items.ItemEnchantmentGem;
+
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -23,65 +29,112 @@ public class HS_AnvilEvent
 		{
 			return;
 		}
-		
-		Map leftEnchantments = EnchantmentHelper.getEnchantments(leftInput);
-		Map rightEnchantments = EnchantmentHelper.getEnchantments(rightInput);
-		Collection leftEnchantmentCollection = leftEnchantments.values();
-		Collection rightEnchantmentCollection = rightEnchantments.values();
-		
-		if(!leftEnchantmentCollection.containsAll(rightEnchantmentCollection))
+
+		Item leftItem = leftInput.getItem();
+		Item rightItem = rightInput.getItem();
+
+		if (rightItem instanceof ItemEnchantmentGem)
 		{
-			leftEnchantments.putAll(rightEnchantments);
-			//ItemStack result = new ItemStack(leftInput.getItem(), 1, leftInput.getItemDamage());
-			ItemStack result = leftInput.copy();
-			EnchantmentHelper.setEnchantments(leftEnchantments, result);
-			
-			event.output = result;
-			event.cost = 1;
+			handleAnvilGem(leftInput, leftItem, rightInput, rightItem, event);
 		}
+		//
+		// Map leftEnchantments = EnchantmentHelper.getEnchantments(leftInput);
+		// Map rightEnchantments =
+		// EnchantmentHelper.getEnchantments(rightInput);
+		// Collection leftEnchantmentCollection = leftEnchantments.values();
+		// Collection rightEnchantmentCollection = rightEnchantments.values();
+		//
+		// if(!leftEnchantmentCollection.containsAll(rightEnchantmentCollection))
+		// {
+		// leftEnchantments.putAll(rightEnchantments);
+		// ItemStack result = leftInput.copy();
+		// EnchantmentHelper.setEnchantments(leftEnchantments, result);
+		//
+		// event.output = result;
+		// event.cost = 1;
+		// }
 	}
 
-	public static int getEnchantmentCost(ItemStack stack)
+	public void handleAnvilGem(ItemStack leftInput, Item leftItem, ItemStack rightInput, Item rightItem, AnvilUpdateEvent event)
 	{
-		// derived from ContainerRepair
-		Map enchantmentMap = EnchantmentHelper.getEnchantments(stack);
-		Iterator iterator = enchantmentMap.keySet().iterator();
-		while (iterator.hasNext())
+		if (!(leftItem instanceof ItemEnchantmentGem) && rightItem instanceof ItemEnchantmentGem)
 		{
-			int currentEnchant = ((Integer) iterator.next()).intValue();
-			Enchantment enchantment = Enchantment.enchantmentsList[currentEnchant];
-			int level = ((Integer) enchantmentMap.get(Integer.valueOf(currentEnchant))).intValue();
-			if (enchantment.canApply(stack))
+			ItemEnchantmentGem gem = (ItemEnchantmentGem) rightItem;
+			if (leftInput.isItemEnchanted() && !rightInput.isItemEnchanted())
 			{
-				if (level > enchantment.getMaxLevel())
+				if (leftItem.isItemTool(leftInput) && leftInput.getItemDamage() < (leftInput.getMaxDamage() / 4))
 				{
-					level = enchantment.getMaxLevel();
+					HarderStart.log.log(Level.INFO, "handleEnchantGem called");
+					handleEnchantGem(leftInput, leftItem, rightInput, gem, event);
 				}
-				int costPerLevel = 0;
-				switch (enchantment.getWeight())
-				{
-					case 1:
-						costPerLevel = 8;
-						break;
-					case 2:
-						costPerLevel = 4;
-					case 3:
-					case 4:
-					case 6:
-					case 7:
-					case 8:
-					case 9:
-					default:
-						break;
-					case 5:
-						costPerLevel = 2;
-						break;
-					case 10:
-						costPerLevel = 1;
-				}
-				return (costPerLevel * level);
+			}
+			else if (!leftInput.isItemEnchanted() && rightInput.isItemEnchanted())
+			{
+				HarderStart.log.log(Level.INFO, "handleEnchantTool called");
+				handleEnchantTool(leftInput, leftItem, rightInput, gem, event);
 			}
 		}
-		return 0;
+
+	}
+
+	public void handleEnchantGem(ItemStack toolStack, Item tool, ItemStack gemStack, ItemEnchantmentGem gem, AnvilUpdateEvent event)
+	{
+		event.materialCost = 1;
+		Enchantment enchant = gem.getGemEnchantment();
+		HarderStart.log.log(Level.INFO, EnchantmentHelper.getEnchantments(toolStack));
+
+		Map enchantMap = EnchantmentHelper.getEnchantments(toolStack);
+
+		int enchantID = enchant.effectId;
+		if (EnchantmentHelper.getEnchantments(toolStack).containsKey(enchantID))
+		{
+			int level = ((Integer) enchantMap.get(enchantID));
+			HarderStart.log.log(Level.INFO, "contains enchant: " + enchant);
+			event.cost = getEnchantmentCost(enchant, level);
+			event.materialCost = 1;
+			ItemStack outputStack = new ItemStack(gem);
+			outputStack.addEnchantment(enchant, level);
+			event.output = outputStack;
+			
+		}
+
+	}
+
+	public void handleEnchantTool(ItemStack toolStack, Item tool, ItemStack gemStack, ItemEnchantmentGem gem, AnvilUpdateEvent event)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int getEnchantmentCost(Enchantment enchant, int level)
+	{
+		if (level > enchant.getMaxLevel())
+		{
+			level = enchant.getMaxLevel();
+		}
+		int costPerLevel = 0;
+		switch (enchant.getWeight())
+		{
+			case 1:
+				costPerLevel = 8;
+				break;
+			case 2:
+				costPerLevel = 4;
+			case 3:
+			case 4:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			default:
+				break;
+			case 5:
+				costPerLevel = 2;
+				break;
+			case 10:
+				costPerLevel = 1;
+		}
+		return (costPerLevel * level);
+
 	}
 }
